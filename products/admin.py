1576e98a2300fbabd9cff7b8737fad3902a565c7
mutisyamusembi <56022113+mutisyamusembi@users.noreply.github.com>
2020-02-01 08:49:22 +0300

    Add files via upload

---
+++ products/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Product, Offers
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+class OffersAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+admin.site.register(Offers, OffersAdmin)
+admin.site.register(Product, ProductAdmin )
